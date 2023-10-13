69b154b3cc1630eb69d2f107d1585332521f3e91
Evelyn <evelynliquez04@outlook.com>
2023-10-13 10:29:11 -0600

    Programacion de Program y Rol del archivo Models

--- FrontendAdminCatalogoLibros.AppBlazor/Program.cs
+++ FrontendAdminCatalogoLibros.AppBlazor/Program.cs
@@ -1,11 +1,23 @@
+using Blazored.LocalStorage;
 using FrontendAdminCatalogoLibros.AppBlazor;
+using FrontendAdminCatalogoLibros.AppBlazor.Auth;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7233/api/") });
+
+builder.Services.AddMudServices();
+
+builder.Services.AddScoped<UserAuth>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationProvider>();
+builder.Services.AddAuthorizationCore();
+
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
